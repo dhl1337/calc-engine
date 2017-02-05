5194650598394b9b066cdcefcd926f61c322e002
Dan Le <dhl1337@gmail.com>
2017-02-05 01:27:31 -0700

    practicing logical and conditional

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,6 +3,25 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double var1 = 100.0d;
+        double val2 = 0.0d;
+        double result;
+
+        char opCode = 'd';
+
+        if (opCode == 'a') {
+            result = var1 + val2;
+        } else if (opCode == 's') {
+            result = var1 - val2;
+        } else if (opCode == 'd') {
+            result = val2 != 0.0d ? var1 / val2 : 0.0d;
+        } else if (opCode == 'm') {
+            result = var1 * val2;
+        } else {
+            System.out.println("Error - invalide code");
+            result = 0.0d;
+        }
+
+        System.out.println(result);
     }
 }
